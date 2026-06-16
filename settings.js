eb168a34a0d2e508129135ac1f80054531d25dd6
ANICADE Tech <tresforzulu@gmail.com>
2026-06-16 15:29:24 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "JINX-MD",
   botOwner: 'ANICADE Tech', // Your name
-  ownerNumber: '260955078098', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '260769563483', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
